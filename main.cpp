11c55b553e2702e3b864a52d533b9dc6fc0ba5b9
smslsz <2138799740lsz@gmail.com>
2025-09-16 11:23:14 +0800

    fc

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+    int kk;
+    cin>>kk;
+    int arr[kk];
+    int dp[kk];
+    for(int i=0;i<kk;i++){
+        cin>>arr[i];
+        dp[i]=1;
+    }
+    int ans=-1;
+    for(int i=1;i<kk;i++){
+        for(int j=0;j<i;j++) {
+            if(arr[j]<arr[i]) {
+                dp[i]=max(dp[i], dp[j]+1);
+            }
+        }
+        ans=max(ans, dp[i]);
+    }
+    cout<<ans<<'\n';
+    return 0;
+}
